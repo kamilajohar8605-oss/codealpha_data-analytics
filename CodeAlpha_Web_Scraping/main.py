a98e6272501cbcb1081fbc7de70ac3cb3a7aec54
kamilajohar8605-oss <kamilajohar8605@gmail.com>
2026-06-12 11:59:48 +0530

    Add files via upload

---
+++ CodeAlpha_Web_Scraping/main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://quotes.toscrape.com/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+quotes = []
+
+for quote in soup.find_all("span", class_="text"):
+    quotes.append(quote.text)
+
+df = pd.DataFrame(quotes, columns=["Quotes"])
+df.to_csv("quotes.csv", index=False)
+
+print("Output saved to quotes.csv")
